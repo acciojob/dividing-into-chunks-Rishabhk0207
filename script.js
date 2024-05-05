329f012e3e678b4992e22b9da88d788ea65d3add
Rishabh kumar <83425253+Rishabhk0207@users.noreply.github.com>
2024-05-05 22:34:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,24 +2,23 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
-let ans = [];
+ let ans = [];
   let sum = 0;
-  let z = 0; // Initialize z to 0
+  let z = 0; 
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
     if (sum > n) {
-      ans.push(arr.slice(z, i)); // Slice from z to i (exclusive)
-      z = i; // Update z for the next subarray
-      sum = 0; // Reset sum
-    }
-  } 
-  // Check if there are remaining elements to form the last subarray
+      ans.push(arr.slice(z, i)); 
+      z = i;
+      sum = arr[i]; 
+  }
+
   if (z < arr.length) {
-    ans.push(arr.slice(z)); // Slice from z to the end of the array
+    ans.push(arr.slice(z)); 
   }
   return ans;
 	 
 };
   
-const n = prompt("Enter n: ");  
-alert(JSON.stringify(divide(arr, n)));  
+// const n = prompt("Enter n: ");  
+// alert(JSON.stringify(divide(arr, n)));  
